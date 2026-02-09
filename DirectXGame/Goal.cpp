c8c4516f874a61a239a714803202590395730dcf
HamamatuReiya <k022g0050@g.neec.ac.jp>
2026-02-09 14:38:02 +0900

    当たり判定修正

--- DirectXGame/Goal.cpp
+++ DirectXGame/Goal.cpp
@@ -4,7 +4,7 @@ void Goal::Initialize(uint32_t textureHandle, Vector2 pos) {
 	position_ = pos;
 	sprite_ = Sprite::Create(
 	    textureHandle, {position_.x, position_.y}, {1.0f, 1.0f, 1.0f, 1.0f}, // 色
-	    {0.0f, 0.0f}                                                         // アンカーポイント
+	    {1.0f, 1.0f}                                                         // アンカーポイント
 	);
 
 	sprite_->SetSize(size_);
@@ -14,4 +14,8 @@ void Goal::Update() {
 	// 特に動かない場合は空
 }
 
-void Goal::Draw() { sprite_->Draw(); }
+void Goal::Draw() {
+	if (sprite_) {
+		sprite_->Draw();
+	}
+}
